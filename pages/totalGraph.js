dcdb237d99bd098c23ec0265eae99f3f1c6ecb2c
Irving Vanegas <l17121069@morelia.tecnm.mx>
2022-12-21 05:32:21 -0600

    .

--- pages/totalGraph.js
+++ pages/totalGraph.js
@@ -187,7 +187,7 @@ const TotalGraph = (props) => {
                                 Desviacion<br/>Estandar
                             </Button>
                             <Button colorScheme='teal' size='lg'>
-                                Operacion
+                                Guardar
                             </Button>
                         </Stack>
                 </Container>
